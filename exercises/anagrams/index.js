cb341fde86aa5179b213c1caf97b065ca17e268e
Rafał Panasiuk <panasiukraf@gmail.com>
2018-04-12 23:19:42 +0200

    Algorithms completed.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -5,9 +5,42 @@
 // or punctuation.  Consider capital letters to be the same as lower case
 // --- Examples
 //   anagrams('rail safety', 'fairy tales') --> True
-//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> FALSE
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function charsMap(str) {
+// 	const str_ = str.replace(' ', '');
+// 	let charMap = {};
+// 	for (let char of str_) {
+// 		charMap[char] = charMap[char] + 1 || 1;
+// 	};
+// 	return charMap;
+// }
 
+// function anagrams(stringA, stringB) {
+// 	const charMapA = charsMap(stringA);
+// 	const charMapB = charsMap(stringB);
+// 	if (Object.keys(charMapA).length === Object.keys(charMapB).length) {
+// 		for (let char in charMapA) {
+// 			// debugger;
+// 			if (charMapA[char] !== charMapB[char]) {
+// 				return false;
+// 			};
+// 		};
+// 	} else {
+// 		return false;
+// 	};
+// 	return true;
+// }
+
+function anagrams(stringA, stringB) {
+	return cleanString(stringA) === cleanString(stringB);
+};
+
+function cleanString(str) {
+	return str.replace(/[^\w]/g, '').split('').sort().join('');
+};
+// charsMap('BYE there');
+// charsMap('bye there');
+// anagrams('BYE there', 'bye there');
 module.exports = anagrams;
