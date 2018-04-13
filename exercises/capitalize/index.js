98f8ca6b9eb3bb74a2d013098b5376ded0e38070
Rafał Panasiuk <panasiukraf@gmail.com>
2018-04-13 19:24:14 +0200

    Algorithms completed.

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,41 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+// 	const capitalized = [];
+// 	for (let char of str.split(' ')) {
+// 		let upper_char = char[0].toUpperCase();
+// 		capitalized.push(upper_char + char.slice(1));
+// 	};
+// 	return capitalized.join(' ');
+// }
 
+function capitalize(str) {
+	var capQuestionMark = capitalQuestionMark(str);
+	let capitalized = capQuestionMark[0];
+	const iter_start = capQuestionMark[1];
+	// debugger;
+	for (let i = iter_start; i < str.length; i++) {
+		if (str[i - 1] == ' ') {
+			capitalized += str[i].toUpperCase();
+		} else {
+			capitalized += str[i];
+		};
+	};
+	return capitalized;
+}
+
+function capitalQuestionMark(str) {
+	if (str[0] === '?') {
+		const capitalized = str[0] + str[1].toUpperCase();
+		const iter_start = 2;
+		return [capitalized, iter_start];
+	} else {
+		const capitalized = str[0].toUpperCase();
+		const iter_start = 1;
+		return [capitalized, iter_start];
+	};
+}
+// capitalQuestionMark('?que pasa')
+// capitalize('?que pasa')
 module.exports = capitalize;
