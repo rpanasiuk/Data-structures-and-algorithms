7de0fd89dc9a9eebf42ffda3bab331abc0c81825
Rafał Panasiuk <panasiukraf@gmail.com>
2018-04-14 22:40:51 +0200

    Algorithm completed.

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+// 	const top_lvl = Math.floor((2*n - 1) / 2);
+
+// 	for (i = 0; i < n; i++) {
+// 		let lvl = '';
+// 		for (let j = 0; j < 2*n - 1; j++) {
+// 			if (top_lvl - i <= j && top_lvl + i >= j) {
+// 				lvl += '#';
+// 			} else {
+// 				lvl += ' ';
+// 			};
+// 		}
+// 		console.log(lvl);
+// 	};
+// }
+
+function pyramid(n, row=0, level='') {
+	if (n == row) {
+		return;
+	}
+	if (level.length === 2 * n - 1) {
+		console.log(level);
+		return pyramid(n, row + 1);
+	};
+	const midpoint = Math.floor((2 * n - 1) / 2);
+	let add;
+	if (midpoint - row <= level.length &&
+	 	midpoint + row >= level.length) {
+		add = '#';
+	} else {
+		add = ' ';
+	};
+	pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
