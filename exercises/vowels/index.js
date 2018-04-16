cec74157e7962c6e0bebb29dbb8a92d02c0eadbc
Rafał Panasiuk <panasiukraf@gmail.com>
2018-04-16 21:37:06 +0200

    Algorithms completed.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+// 	let counter = 0;
+// 	const arr = ['a', 'e', 'i', 'o', 'u'];
+
+// 	for (let chars of str.toLowerCase()) {
+// 		if (arr.includes(chars)) {
+// 			counter++;
+// 		};
+// 	};
+// 	return counter;
+// }
+
+function vowels(str) {
+	const matches = str.match(/[aeiou]/gi);
+	return matches? matches.length : 0;
+}
 
 module.exports = vowels;
