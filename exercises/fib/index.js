5a196ecda0cc374fa3053f5010d3e0599797b24d
Rafał Panasiuk <panasiukraf@gmail.com>
2018-04-17 23:08:36 +0200

    Algorithm completed.

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+// 	const fibo = [0, 1];
+// 	for (let i = 2; i <= n; i++) {
+// 		const a = fibo[i - 1];
+// 		const b = fibo[i - 2];
+// 		fibo.push(a + b);
+// 	};
+// 	return fibo[fibo.length - 1];
+// }
+
+function memoize(fn) {
+	const cache = {};
+	return function(...args) {
+		if (cache[args]) {
+			return cache[args];
+		};
+		const result = fn.apply(this, args);
+		cache[args] = result;
+
+		return result;
+	}	;
+}
+
+function fib(n) {
+	if (n < 2) {
+		return n;
+	}
+
+	return fib(n-1) + fib(n-2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
