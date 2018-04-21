146207fe976573a1a64121356a6ad94f96bc2b3d
Rafał Panasiuk <panasiukraf@gmail.com>
2018-04-21 23:42:50 +0200

    Algorithms completed.

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+	constructor() {
+		this.array = [];
+	};
+
+	push(record) {
+		this.array.push(record);
+	};
+
+	pop() {
+		return this.array.pop();
+	};
+
+	peek() {
+		return this.array[this.array.length - 1];
+	}
+}
 
 module.exports = Stack;
