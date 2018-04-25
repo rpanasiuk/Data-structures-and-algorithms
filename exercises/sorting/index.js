6682e0bfb83ca2faf8e5b3108484c046f929fdfb
Rafał Panasiuk <panasiukraf@gmail.com>
2018-04-25 22:19:16 +0200

    Added merge sort.

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -48,11 +48,28 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+	if (arr.length == 1) {
+		return arr;
+	}
 
+	var center = Math.floor(arr.length / 2);
+	var left = arr.slice(0, center);
+	var right = arr.slice(center);
+
+	return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+	const result = [];
 
+	while (left.length && right.length) {
+		if (left[0] < right[0]) {
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
+	return [...result, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
